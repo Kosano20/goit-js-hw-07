5c5ef18f4745eb5288968d038b91bd9be618acb9
Kosano <bogdan.kosyak@ukr.net>
2021-09-03 10:35:07 +0300

    ended 7 hw

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,4 +1,37 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const gallery = document.querySelector(".gallery");
+const galleryCreated = createGallery(galleryItems);
+const galleryLink = document.querySelectorAll(".gallery__link");
+gallery.insertAdjacentHTML("beforeend", galleryCreated);
+gallery.addEventListener("click", galleryClick);
+for (const link of galleryLink) {
+  link.addEventListener("click", (event) => {
+    event.preventDefault();
+  });
+}
+function createGallery(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      gallery.insertAdjacentHTML(
+        "beforeend",
+        `<div class = "gallery__item"><a class="gallery__link" href=${original}><img class = "gallery__image"src = ${preview} data-source=${original} alt = "${description}"></a></div> `
+      );
+    })
+    .join("");
+}
+function galleryClick(event) {
+  const galleryPic = event.target;
+  const orgPicture = galleryPic.dataset.source;
+  if (!galleryPic.classList.contains("gallery__image")) {
+    return;
+  }
 
-console.log(galleryItems);
+  basicLightbox
+    .create(
+      `
+    <img src= ${orgPicture} alt =${galleryPic.getAttribute("alt")} >
+`
+    )
+    .show();
+}
